aa5b881fae90f4e9235c07ba8721429176d9471c
eternnoir <eternnoir@gmail.com>
2013-06-14 21:39:07 +0800

    add search function

--- src/com/example/newone/POIService.java
+++ src/com/example/newone/POIService.java
@@ -1,5 +1,16 @@
 package com.example.newone;
 
+import java.util.ArrayList;
+
 public class POIService {
+	
+	public POIService(){
+		
+	}
+	public ArrayList<POI> getNearByPOIs(double lat, double lan, String target){
+		ArrayList<POI> _ret = new ArrayList<POI>();
+		//search poi by location. Use Httpservices & google Place API.
+		return _ret;
+	}
 
 }

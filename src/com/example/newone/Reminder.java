b0bc008b49fd9a2daafa6516cbe75cb8428c1082
Lala <gingkoginkgo@hotmail>
2013-06-15 00:03:40 +0800

    addReminder

--- src/com/example/newone/Reminder.java
+++ src/com/example/newone/Reminder.java
@@ -1,13 +1,36 @@
 package com.example.newone;
 
 import android.app.Activity;
-
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
 
 //如果確定提醒，交給他處理
 
 public class Reminder extends Activity {
 
+	private WebView ShowTargeMapForUser;
+	public Reminder(){}
+		
+	@Override
+		protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.showtargetmap);
+	    ShowTargeMapForUser = (WebView)findViewById(R.id.webview);
+	    WebSettings webSettings = ShowTargeMapForUser.getSettings();
+	    webSettings.setJavaScriptEnabled(true);
+		}
 	
+	  @Override
+	  public void onStart()
+	  {
+	    super.onStart();
+	    String ShowResUrl = "http://ascii.net/ok.jsp";
+	    ShowTargeMapForUser.loadUrl(ShowResUrl);
+	    ShowTargeMapForUser.setWebViewClient(new WebViewClientImpl());
+	  }
+		
+	}
 	
-	
-}
+

25af020ab42ebe3537e17ac4d4524a224fee5b9b
eternnoir <eternnoir@gmail.com>
2013-06-14 21:38:02 +0800

    add POI class

---
+++ src/com/example/newone/POI.java
@@ -0,0 +1,23 @@
+package com.example.newone;
+
+public class POI {
+	private String _name;
+	private double _lat;
+	private double _lng;
+	
+	public POI(String name, double lat, double lng){
+		_name = name;
+		_lat = lat;
+		_lng = lng;
+	}
+	
+	public String getName(){
+		return _name;
+	}
+	public double getLat(){
+		return _lat;
+	}
+	public double getLng(){
+		return _lng;
+	}
+}

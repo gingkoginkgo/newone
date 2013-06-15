89b4a38c6471257aa9225454ecf2ad8a80f93286
Lala <gingkoginkgo@hotmail>
2013-06-15 23:37:36 +0800

    nothing

--- src/com/example/newone/Reminder.java
+++ src/com/example/newone/Reminder.java
@@ -3,6 +3,7 @@ package com.example.newone;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
@@ -26,34 +27,25 @@ public class Reminder extends Activity {
 		protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		
 		//取得Notification服務
-		NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-		
+		NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);	
 		//設定當按下這個通知之後要執行的activity 囧!
-		
 		 Intent notify_Intent = new Intent(Reminder.this,Reminder.class);
          notify_Intent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK);
-         PendingIntent appIntent = PendingIntent.getActivity(Reminder.this,
-         												     0,
-                                                             notify_Intent,
-                                                             0);
-         Notification notification = new Notification();
-		  
+         PendingIntent appIntent = PendingIntent.getActivity(Reminder.this,0,notify_Intent,0);
+         Notification notification = new Notification();  
         //設定出現在狀態列的圖示  
         notification.icon=R.drawable.ic_launcher;
         //顯示在狀態列的文字
-        notification.tickerText="這附近有可處理的待辦事項";
-        
+        notification.tickerText="這附近有可處理的待辦事項";     
         //設定通知的標題、內容
         notification.setLatestEventInfo(Reminder.this,"ToDoList","在哪裡呢?",appIntent);
         //送出Notification
         notificationManager.notify(1,notification);
-		  
 		
-		
-		
-		
-		
+        
+      
 		//顯示地圖
 		//////////////////////////////////////////////////////////////////////
 	    setContentView(R.layout.showtargetmap);
@@ -66,7 +58,8 @@ public class Reminder extends Activity {
 		MAP_URL="https://maps.google.com.tw/maps?q="+recive_resultLat+","+recive_resultLng;
 	    ShowTargeMapForUser.loadUrl(MAP_URL);
 	    //////////////////////////////////////////////////////////////////////
-	  } 
+        	
+        } 
 
 }
 	

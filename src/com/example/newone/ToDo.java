d81e7b0a0394f1f8d490522dc4e929bccb062eb9
eternnoir <eternnoir@gmail.com>
2013-06-19 16:25:47 +0800

    let ToDo Comparable, for sort

--- src/com/example/newone/ToDo.java
+++ src/com/example/newone/ToDo.java
@@ -1,12 +1,13 @@
 package com.example.newone;
 
-public class ToDo {
+public class ToDo implements Comparable{
 	private String ActionType;
 	private String StartTime;
 	private long DeadlineTime;
 	private String Description;
 	private String Target_Place;
 	private float Priority_Rate;
+	private float _score;
 	
 	public ToDo(String _ActionType , String _StartTime , long _DeadlineTime , String _Description, String _Target_Place,Float _rate)
 	{
@@ -16,6 +17,7 @@ public class ToDo {
 		Description = _Description;
 		Target_Place = _Target_Place;
 		Priority_Rate = _rate;
+		_score = _rate;
 	}
 	
 	public String getActionType(){
@@ -35,5 +37,15 @@ public class ToDo {
 	}
 	public float getPriority_Rate(){
 		return Priority_Rate;
+	}
+	public float getScore(){
+		return _score;
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		ToDo tmp = (ToDo)arg0;
+		float ret = this._score - tmp.getScore();
+		return (int)ret;
 	}
 }
